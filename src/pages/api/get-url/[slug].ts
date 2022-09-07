badced5ca97e7925ef20761445f52203b63f29ba
Tawanda <ejmunongo@gmail.com>
2022-09-07 12:16:04 +0200

    add caching for speed

--- src/pages/api/get-url/[slug].ts
+++ src/pages/api/get-url/[slug].ts
@@ -22,6 +22,10 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (!data) {
     res.statusCode = 404;
 
+    res.setHeader("Content-Type", "application/json");
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Cache-Control", "s-maxage=1000000, stale-while-revalidate");
+
     res.send(JSON.stringify({ message: "this slug was not found" }));
 
     return;
